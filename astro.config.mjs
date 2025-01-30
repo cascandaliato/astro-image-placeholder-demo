8b01dd19d1076665b7619b1fc428b19313b1e7f5
Carmelo Scandaliato <8927157+cascandaliato@users.noreply.github.com>
2025-01-30 22:48:56 +0000

    first commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+
+// https://astro.build/config
+export default defineConfig({
+  site: "https://cascandaliato.github.io",
+  base: "astro-image-placeholder-demo",
+});
